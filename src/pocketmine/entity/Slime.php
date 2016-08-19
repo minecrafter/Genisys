a2b74c2c538110dabbb99cb0f5630ee1427a7111
dktapps <odigiman@gmail.com>
2016-08-19 08:59:36 +0100

    Random spawn sizes for Slimes and Magma Cubes
    /summon for extra large slimes currently doesn't work. TODO: fix /summon
    command. Typical.

--- src/pocketmine/entity/Slime.php
+++ src/pocketmine/entity/Slime.php
@@ -23,12 +23,19 @@ namespace pocketmine\entity;
 
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\item\Item as ItemItem;
+use pocketmine\level\format\FullChunk;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
 use pocketmine\Player;
 
 class Slime extends Living{
 	const NETWORK_ID = self::SLIME;
 
 	const DATA_SLIME_SIZE = 16;
+	
+	const SIZE_TINY = 0; //1
+	const SIZE_SMALL = 1; //2
+	const SIZE_BIG = 3; //4
 
 	public $width = 0.3;
 	public $length = 0.9;
@@ -36,27 +43,43 @@ class Slime extends Living{
 
 	public $dropExp = [1, 4];
 	
+	public function __construct(FullChunk $chunk, CompoundTag $nbt){
+		if(!isset($nbt["Size"])){
+			$nbt->Size = new IntTag("Size", self::getRandomSize());
+		}
+		parent::__construct($chunk, $nbt);
+	}
+	
+	public function initEntity(){
+		parent::initEntity();
+		if(!isset($this->namedtag["Size"])){
+			$this->namedtag->Size = new IntTag("Size", self::getRandomSize());
+		}
+		$this->setDataProperty(self::DATA_SLIME_SIZE, self::DATA_TYPE_INT, $this->getSize());
+	}
+	
 	public function getName() : string{
 		return "Slime";
 	}
+
+	public function getSize(): int{
+		return $this->namedtag["Size"];
+	}
 	
-	public function getDrops(){
-		$drops = array(ItemItem::get(ItemItem::SLIMEBALL, 0, 1));
-		if ($this->lastDamageCause instanceof EntityDamageByEntityEvent and $this->lastDamageCause->getEntity() instanceof Player) {
-			if (\mt_rand(0, 199) < 5) {
-				switch (\mt_rand(0, 2)) {
-					case 0:
-						$drops[] = ItemItem::get(ItemItem::IRON_INGOT, 0, 1);
-						break;
-					case 1:
-						$drops[] = ItemItem::get(ItemItem::CARROT, 0, 1);
-						break;
-					case 2:
-						$drops[] = ItemItem::get(ItemItem::POTATO, 0, 1);
-						break;
-				}
-			}
+	public function setSize(int $size){
+		$size &= 255;
+		$this->namedtag->Size->setValue($size); //255 is the biggest possible size
+		$this->setDataProperty(self::DATA_SLIME_SIZE, self::DATA_TYPE_INT, $size);
+	}
+	
+	public static function getRandomSize(): int{
+		$rand = mt_rand(0, 100);
+		if($rand <= 16){
+			return self::SIZE_TINY;
+		}elseif($rand <= 49){
+			return self::SIZE_SMALL;
+		}else{
+			return self::SIZE_BIG;
 		}
-		return $drops;
 	}
 }
